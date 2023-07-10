09ff7fac1184c3d8c56d0db94d0dd4f25ba10a5d
heyhoe <h3y6e@pm.me>
2023-07-10 18:26:01 +0900

    add a script to delete feedgen

---
+++ scripts/deleteFeedGen.ts
@@ -0,0 +1,44 @@
+import dotenv from 'dotenv'
+import { AtpAgent } from '@atproto/api'
+import { ids } from '../src/lexicon/lexicons'
+
+const run = async () => {
+  dotenv.config()
+
+  // YOUR bluesky handle
+  // Ex: user.bsky.social
+  const handle = 'h3y6e.bsky.social'
+
+  // YOUR bluesky password
+  if (!process.env.FEEDGEN_PUBLISH_APP_PASSWORD) {
+    throw new Error('Please provide an app password in the .env file')
+  }
+  const password = process.env.FEEDGEN_PUBLISH_APP_PASSWORD
+
+  // A short name for the record that will show in urls
+  // Lowercase with no spaces, max 15 chars
+  // Ex: whats-hot
+  const recordName = 'sandbox'
+
+  // only update this if in a test environment
+  const agent = new AtpAgent({ service: 'https://bsky.social' })
+  await agent.login({ identifier: handle, password })
+
+  try {
+    await agent.api.app.bsky.feed.describeFeedGenerator()
+  } catch (err) {
+    throw new Error(
+      'The bluesky server is not ready to accept published custom feeds yet',
+    )
+  }
+
+  await agent.api.com.atproto.repo.deleteRecord({
+    repo: agent.session?.did ?? '',
+    collection: ids.AppBskyFeedGenerator,
+    rkey: recordName,
+  })
+
+  console.log('All done 🎉')
+}
+
+run()
